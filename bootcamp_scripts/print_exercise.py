92a8e2c4682d03f0334f7ecfec9233392159c8d9
dredaz3 <Andre.Nelson1@gmail.com>
2020-09-24 18:12:57 -0400

    update

---
+++ bootcamp_scripts/print_exercise.py
@@ -0,0 +1,17 @@
+print("hi this is your computer speaking")
+print(" I can run a lot of python code")
+
+firstname = "Andre"
+# print(firstname)
+print(1+3)
+print(1*3)
+print(1-3)
+
+print(1.1 + 3.1)
+print(1.5 * 2)
+print(1-3.736)
+
+print(int(3.0))
+print(int(5.8))
+print(int(2))
+help()
