d0a0eeeffcc1d86b3d30b9949fa4e8583d5e7492
dredaz3 <Andre.Nelson1@gmail.com>
2020-09-25 02:43:01 -0400

    testocmmit

--- workhours.py
+++ workhours.py
@@ -5,8 +5,21 @@ nonoffice =["William", "Paul"] #if non worker 90% is tier 2Supplementary Activit
 nonofficerate1 = (.90 *hours)#this is the middle tier
 nonofficerate2 = (.1 * hours) #this is the top tier
 
+hours_q1 = [335.5, 3.5, 249.5, 61.5, 502.5]
+hours_q2 = [303.75, 32.5, 221.5, 372.5]
+hours_q3 = [269, 233, 148]
+hours_q4 = [120.5, 43, 61, 121]
+
+rate = 11, 12, 12, 12, 14, 19
+
 total = hours * rate 
 
-def formula(hours, rate, class_):
-    amount = hours * rate * class_
+def formula(hours, rate, name):
+    amount = (hours * rate) 
+    newamount_9 = (amount) * .9
+    newamount_1 =(amount) * .1
+    return amount, newamount_1, newamount_9, rate, name, 
 
+formula(335, 19, "Lori")
+#sample Q1
+hours_q1[:3]
